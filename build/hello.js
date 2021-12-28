bca239add1ccdd1b8f001748e9b510f3cb9f4d87
hernanborre <borre.hernan@gmail.com>
2021-12-28 11:56:27 -0300

    initial commit

---
+++ build/hello.js
@@ -0,0 +1,21 @@
+"use strict";
+function sumar(num1, num2) {
+    return num1 + num2;
+}
+function imprimirSuma(num1, num2) {
+    //return num1.toString() + num2.toString()
+    return `La suma es: ${num1 + num2}`;
+}
+let texto = "hola como estas?";
+let n1 = 3;
+let n2 = 10;
+let arr = [];
+arr.push(n1);
+arr.push(n2);
+sumar(n1, n2);
+console.log(imprimirSuma(n1, n2));
+let num4 = 10;
+
+let p1 = {
+    edad: 18
+};
---
+++ hello.ts
@@ -0,0 +1,32 @@
+
+
+function sumar(num1: number, num2: number):number{
+    return num1 + num2
+}
+
+function imprimirSuma(num1: number, num2: number): string {
+    //return num1.toString() + num2.toString()
+    return `La suma es: ${num1 + num2}` 
+}
+
+let texto: string = "hola como estas?" 
+let n1:number = 3
+let n2:number = 10
+
+let arr:number [] = []
+arr.push(n1)
+arr.push(n2)
+
+sumar(n1, n2)
+console.log(imprimirSuma(n1, n2))
+
+interface Persona {
+    name?: string
+    edad: number
+}
+
+let num4 = 10 
+
+let p1: Persona = {
+    edad: 18 
+}
